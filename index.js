ba07b19caf650b955f1492ea9a86846435fc5d15
20Koen02 <kvanwijn@online.nl>
2019-12-17 14:07:07 +0100

    login done

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
+
 const express = require('express');
-const app = express();
-var http = require('http').Server(app);
+const cookieParser = require('cookie-parser');
+let jwt = require('jsonwebtoken');
+var bodyParser = require('body-parser')
+let middleware = require('./middleware');
+const cors = require('cors');
 
+
+let app = express();
+const port = process.env.PORT || 8003;
+
+app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.set('view engine', 'ejs');
-app.set('view options', {
-    pretty: true
-});
-
-
 app.use(express.static(__dirname + '/views'));
+app.set('trust proxy', true)
 
-app.get('/', async function(req, res) {
-    res.render('pages/login');
-});
 
-http.listen(4040, () => console.log(`App listening on port 4040!`));
+app.post('/login', middleware.generateToken);
+app.get('/login', function (req, res) {
+    res.render('pages/login')
+})
+app.get('/', middleware.checkToken);
+
+
+app.listen(port, () => console.log(`Server is listening on port: ${port}`));
---
+++ middleware.js
@@ -0,0 +1,71 @@
+let jwt = require('jsonwebtoken');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('database.sqlite3');
+const argon2 = require('argon2');
+
+
+async function checkPassword(password, hash) {
+    try {
+        return await argon2.verify(hash, password)
+    } catch {
+        return false
+    }
+}
+
+let generateToken = async (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    if (!username || !password) {
+        return res.redirect('/login');
+    }
+
+    db.all(`SELECT * FROM passwords WHERE username="${username}"`, function (err, rows) {
+        if (rows.length == 0) {
+            return res.redirect('/login');
+        }
+        checkPassword(password, rows[0]["hash"]).then((result) => {
+            if (result) {
+                db.all(`SELECT * FROM jwt`, async function(err, rows) {
+                    let token = jwt.sign({ username: username },
+                        rows[0]["key"],
+                        {
+                            expiresIn: '1d'
+                        }
+                    );
+                    res.cookie('token', token, {
+                        expires: new Date(Date.now() + 86400000)
+                    });
+                    return res.redirect('/');
+                })
+            } else {
+                return res.redirect('/login');
+            }
+        })
+    });
+};
+
+let checkToken = async (req, res) => {
+    const token = req.cookies.token || '';
+    try {
+        if (!token) {
+            return res.redirect('/login');
+        }
+        db.all(`SELECT * FROM jwt`, async function(err, rows) {
+            try {
+                const decrypt = await jwt.verify(token, rows[0]["key"])
+                console.log(req.ip +"\tlogged in at\t" + Date(Date.now()).toString())
+                return res.render('pages/index')
+            } catch (e) {
+                return res.redirect('/login');
+            }
+        })
+    } catch (err) {
+        res.redirect('/login');
+    }
+};
+
+module.exports = {
+    checkToken: checkToken,
+    generateToken: generateToken
+};
